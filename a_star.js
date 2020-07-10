de1ae0d78aab5ce992f796a2fefeb45c6e838887
RutuKendre <kendrerutuja111@gmail.com>
2020-07-10 13:44:51 +0530

    a_star modified

--- a_star.js
+++ a_star.js
@@ -1,126 +1,28 @@
 var BinaryHeap = require('@tyriar/binary-heap');
 
-var rows = 8;
-var columns = 5;
-var srcX=0;
-var srcY=0;
-var destX=7;
-var destY=0;
+
+function AStarFinder(opt) {
+   opt = opt || {};
+   this.allowDiagonal = opt.allowDiagonal;
+   this.heuristic = opt.heuristic || Heuristic.manhattan;
+}
+
+
+
+AStarFinder.prototype.findPath = function(srcX,srcY,destX,destY,grid) {
 var unexploredCellsSet;
+var rows = grid.length;
+var columns = grid[0].length;
+var path = new Array();
 
-function isValidCell(x,y)
-{
-   if(x>=0&&x<rows&&y>=0&&y<columns)
-    return true;
-   else
-    return false;
-}
+var endPoints = {
+   srcX:srcX,
+   srcY:srcY,
+   destX:destX,
+   destY:destY,
+};
 
-function isDestination(x,y)
-{
-  if(x==destX&&y==destY)
-    return true;
-  else
-    return false;
-}
 
-//function manhattan(x,y,destX,destY)
-//function diagonal(x,y,destX,destY)
-
-function printPath(cellDetails)
-{
-  var row = destX;
-  var col = destY;
-
-   //console.log(cellDetails[row][col].parentX,cellDetails[row][col].parentY);
-    console.log('yghg');
-   while (!(cellDetails[row][col].parentX == srcX 
-             && cellDetails[row][col].parentY == srcY )) 
-    { 
-        //Path.push (make_pair (row, col));
-        console.log(row,col); 
-        var temp_row = cellDetails[row][col].parentX; 
-        var temp_col = cellDetails[row][col].parentY; 
-        row = temp_row; 
-        col = temp_col; 
-    } 
-     console.log(row,col); 
-     console.log(srcX,srcY);
-}
-
-function euclidean(x,y)
-{
-  return Math.pow((x-destX)*(x-destX)+(y-destY)*(y-destY),0.5)
-}
-
-function manhattan(x,y)
-{
-  var a = Math.abs(x-destX);
-  var b = Math.abs(y-destY);
-  return a+b;
-}
-
-function diagonal(x,y)
-{
-  var a = Math.abs(x-destX);
-  var b = Math.abs(y-destY);
-  if(a>=b)
-    return a;
-  else
-    return b;
-}
-
-function isUnblocked(grid,x,y)
-{
-   if(grid[x][y]==1)
-    return true;
-   else
-    return false;
-}
-
-function checkneighbour(x,y,cellDetails,foundDest,exploredCells,grid,xOriginal,yOriginal)
-{
-  //console.log(x,y);
-   if(foundDest.value==true)
-    return;
-   if(isValidCell(x,y))
-   {
-     if(isDestination(x,y)==true)
-     {
-         cellDetails[x][y].parentX = xOriginal; 
-         cellDetails[x][y].parentY = yOriginal; 
-
-         console.log('Destination found');
-         printPath (cellDetails); 
-         //write code to print output here
-        // console.log(x,y);
-         foundDest.value = true; 
-         return; 
-     }
-     else if(exploredCells[x][y]==false&&isUnblocked(grid,x,y)==true)
-     {
-      var gNew = cellDetails[xOriginal][yOriginal].g + 1.0; 
-      // var hNew = euclidean(x,y);
-      var hNew =manhattan(x,y);
-      var fNew = gNew+hNew;
-
-        
-       if (cellDetails[x][y].f == Number.MAX_VALUE ||cellDetails[x][y].f > fNew) 
-                {      
-                    unexploredCellsSet.insert(fNew,{x:x,y:y});               
-                    cellDetails[x][y].f = fNew; 
-                    cellDetails[x][y].g = gNew; 
-                    cellDetails[x][y].h = hNew; 
-                    cellDetails[x][y].parentX=xOriginal;
-                    cellDetails[x][y].parentY=yOriginal; 
-                } 
-     }
-
-   }
-}
-
-function search(grid)
-{
   if(srcX==destX&&srcY==destY)
     return "already present at destination";
   var exploredCells = [];
@@ -174,32 +76,127 @@ function search(grid)
       var x = val.value.x;
       var y = val.value.y;
 
-      checkneighbour(x-1,y,cellDetails,foundDest,exploredCells,grid,x,y);
-      checkneighbour(x+1,y,cellDetails,foundDest,exploredCells,grid,x,y);
-      checkneighbour(x-1,y+1,cellDetails,foundDest,exploredCells,grid,x,y);
-      checkneighbour(x-1,y-1,cellDetails,foundDest,exploredCells,grid,x,y);
-      checkneighbour(x+1,y-1,cellDetails,foundDest,exploredCells,grid,x,y);
-      checkneighbour(x+1,y+1,cellDetails,foundDest,exploredCells,grid,x,y);
-      checkneighbour(x,y+1,cellDetails,foundDest,exploredCells,grid,x,y);
-      checkneighbour(x,y-1,cellDetails,foundDest,exploredCells,grid,x,y);
+      checkneighbour(x-1,y,cellDetails,foundDest,exploredCells,grid,x,y,endPoints,unexploredCellsSet);
+      checkneighbour(x+1,y,cellDetails,foundDest,exploredCells,grid,x,y,endPoints,unexploredCellsSet);
+      checkneighbour(x-1,y+1,cellDetails,foundDest,exploredCells,grid,x,y,endPoints,unexploredCellsSet);
+      checkneighbour(x-1,y-1,cellDetails,foundDest,exploredCells,grid,x,y,endPoints,unexploredCellsSet);
+      checkneighbour(x+1,y-1,cellDetails,foundDest,exploredCells,grid,x,y,endPoints,unexploredCellsSet);
+      checkneighbour(x+1,y+1,cellDetails,foundDest,exploredCells,grid,x,y,endPoints,unexploredCellsSet);
+      checkneighbour(x,y+1,cellDetails,foundDest,exploredCells,grid,x,y,endPoints,unexploredCellsSet);
+      checkneighbour(x,y-1,cellDetails,foundDest,exploredCells,grid,x,y,endPoints,unexploredCellsSet);
+   }
+
+   return path;
+};
+
+
+function isValidCell (x,y,grid)
+{
+   if(x>=0&&x<grid.length&&y>=0&&y<grid[0].length)
+    return true;
+   else
+    return false;
+}
+
+function isDestination (x,y,endPoints)
+{
+  if(x==endPoints.destX&&y==endPoints.destY)
+    return true;
+  else
+    return false;
+}
+
+function printPath (cellDetails,endPoints,path)
+{
+  var row = endPoints.destX;
+  var col = endPoints.destY;
+
+   //console.log(cellDetails[row][col].parentX,cellDetails[row][col].parentY);
+    console.log('yghg');
+   while (!(cellDetails[row][col].parentX == endPoints.srcX 
+             && cellDetails[row][col].parentY == endPoints.srcY )) 
+    { 
+        //Path.push (make_pair (row, col));
+        path.push([row,col]); 
+        var temp_row = cellDetails[row][col].parentX; 
+        var temp_col = cellDetails[row][col].parentY; 
+        row = temp_row; 
+        col = temp_col; 
+    } 
+     path.push([row,col]); 
+     //console.log(endPoints.srcX,endPoints.srcY);
+}
+
+function euclidean (x,y,endPoints)
+{
+  return Math.pow((x-endPoints.destX)*(x-endPoints.destX)+(y-endPoints.destY)*(y-endPoints.destY),0.5)
+}
+
+function manhattan (x,y,endPoints)
+{
+  var a = Math.abs(x-endPoints.destX);
+  var b = Math.abs(y-endPoints.destY);
+  return a+b;
+}
+
+function diagonal (x,y,endPoints)
+{
+  var a = Math.abs(x-endPoints.destX);
+  var b = Math.abs(y-endPoints.destY);
+  if(a>=b)
+    return a;
+  else
+    return b;
+}
+
+function isUnblocked (grid,x,y)
+{
+   if(grid[x][y].walkable==1)
+    return true;
+   else
+    return false;
+}
+
+function checkneighbour (x,y,cellDetails,foundDest,exploredCells,grid,xOriginal,yOriginal,endPoints,unexploredCellsSet)
+{
+  //console.log(x,y);
+   if(foundDest.value==true)
+    return;
+   if(isValidCell(x,y,grid))
+   {
+     if(isDestination(x,y,endPoints)==true)
+     {
+         cellDetails[x][y].parentX = xOriginal; 
+         cellDetails[x][y].parentY = yOriginal; 
+
+         console.log('Destination found');
+         printPath (cellDetails,endPoints); 
+         //write code to print output here
+        // console.log(x,y);
+         foundDest.value = true; 
+         return; 
+     }
+     else if(exploredCells[x][y]==false&&isUnblocked(grid,x,y,endPoints)==true)
+     {
+      var gNew = cellDetails[xOriginal][yOriginal].g + 1.0; 
+      // var hNew = euclidean(x,y);
+      var hNew =manhattan(x,y,endPoints);
+      var fNew = gNew+hNew;
+
+        
+       if (cellDetails[x][y].f == Number.MAX_VALUE ||cellDetails[x][y].f > fNew) 
+                {      
+                    unexploredCellsSet.insert(fNew,{x:x,y:y});               
+                    cellDetails[x][y].f = fNew; 
+                    cellDetails[x][y].g = gNew; 
+                    cellDetails[x][y].h = hNew; 
+                    cellDetails[x][y].parentX=xOriginal;
+                    cellDetails[x][y].parentY=yOriginal; 
+                } 
+     }
+
    }
 }
 
-function mainFunction()
-{
-  
-  var grid =[
-        [ 1, 1, 1, 1, 1 ], 
-        [ 1, 1, 1, 0, 1 ], 
-        [ 1, 0, 0, 0, 1 ], 
-        [ 1, 0, 1, 0, 1 ], 
-        [ 1, 0, 1, 0, 1 ], 
-        [ 1, 0, 1, 0, 1 ], 
-        [ 0, 0, 1, 0, 1 ], 
-        [ 1, 1, 1, 1, 1 ] 
-        ];
-        search(grid);
-}
+module.exports = AStarFinder;
 
-mainFunction();
-console.log('Function called');
